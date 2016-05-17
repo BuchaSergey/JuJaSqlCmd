2e44bf2ef93a1f659cda722afd459f9c928580c9
Sergio <buchasergey03@gmail.com>
2016-05-17 18:29:18 +0300

    Refactoring testov

--- src/test/java/ua.com.juja.sqlcmd/controller/command/FindTest.java
+++ src/test/java/ua.com.juja.sqlcmd/controller/command/FindTest.java
@@ -25,46 +25,52 @@ public class FindTest {
     public void setup() {
         manager = mock(DatabaseManager.class);
         view = mock(View.class);
-       command =  new Find(manager, view);
+        command = new Find(manager, view);
+        command = new Find(manager, view);
     }
 
     @Test
-    public  void  testPrintTableData() {
+    public void testPrintTableData() {
         //given
         when(manager.getTableColumns("user"))
-                .thenReturn(new String[] {"id","name","password"});
+                .thenReturn(new String[]{"id", "name", "password"});
 
         DataSet user1 = new DataSet();
-        user1.put("id",12);
-        user1.put("name","Stiven");
-        user1.put("password","*****");
+        user1.put("id", 12);
+        user1.put("name", "Stiven");
+        user1.put("password", "*****");
 
         DataSet user2 = new DataSet();
-        user2.put("id",13);
-        user2.put("name","Eva");
-        user2.put("password","+++++");
+        user2.put("id", 13);
+        user2.put("name", "Eva");
+        user2.put("password", "+++++");
 
-        DataSet[] data = new DataSet[] {user1, user2};
+        DataSet[] data = new DataSet[]{user1, user2};
         when(manager.getTableData("user"))
                 .thenReturn(data);
         //when
         command.process("find|user");
 
         //then
+
+        shouldPrint("[--------------------, " +
+                    "|id|name|password|, " +
+                    "--------------------, " +
+                    "|12|Stiven|*****|, " +
+                    "|13|Eva|+++++|, " +
+                    "--------------------]");
+    }
+
+    private void shouldPrint(String expected) {
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-     verify(view, atLeastOnce()).write(captor.capture());
-        assertEquals("[--------------------, " +
-                "|id|name|password|, " +
-                "--------------------, " +
-                "|12|Stiven|*****|, " +
-                "|13|Eva|+++++|, " +
-                "--------------------]", captor.getAllValues().toString());
+        verify(view, atLeastOnce()).write(captor.capture());
+        assertEquals(expected, captor.getAllValues().toString());
     }
 
     @Test
     public void testCanProcessFindWithParametersString() {
         // given
-        Command command =  new Find(manager, view);
+        Command command = new Find(manager, view);
 
         // when
         boolean canProcess = command.canProcess("find|user");
@@ -76,7 +82,7 @@ public class FindTest {
     @Test
     public void testCantProcessFindWithOutParametersString() {
         // given
-        Command command =  new Find(manager, view);
+        Command command = new Find(manager, view);
 
         // when
         boolean canProcess = command.canProcess("find");
@@ -96,12 +102,12 @@ public class FindTest {
         // then
         assertFalse(canProcess);
     }
+
     @Test
-    public  void  testPrintEmptyTableData() {
+    public void testPrintEmptyTableData() {
         //given
         when(manager.getTableColumns("user"))
-                .thenReturn(new String[] {"id","name","password"});
-
+                .thenReturn(new String[]{"id", "name", "password"});
 
 
         DataSet[] data = new DataSet[0];
@@ -111,11 +117,9 @@ public class FindTest {
         command.process("find|user");
 
         //then
-        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        verify(view, atLeastOnce()).write(captor.capture());
-        assertEquals("[--------------------, " +
-                "|id|name|password|, " +
-                "--------------------, " +
-                "--------------------]", captor.getAllValues().toString());
+        shouldPrint("[--------------------, " +
+                    "|id|name|password|, " +
+                    "--------------------, " +
+                    "--------------------]");
     }
 }
