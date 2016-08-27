4c8c11d259482fc54cc40f16701f0477005d9e6c
Sergio <buchasergey03@gmail.com>
2016-08-27 11:26:36 +0300

    100% UnsupportedTest

---
+++ src/test/java/ua.com.juja.sqlcmd/controller/command/UnsupportedTest.java
@@ -0,0 +1,42 @@
+package ua.com.juja.sqlcmd.controller.command;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import ua.com.juja.sqlcmd.model.DatabaseManager;
+import ua.com.juja.sqlcmd.view.View;
+
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by Seriy on 27.08.2016.
+ */
+public class UnsupportedTest {
+    DatabaseManager manager;
+    View view;
+    Command command;
+
+    @Before
+    public void setup() {
+        manager = Mockito.mock(DatabaseManager.class);
+        view = Mockito.mock(View.class);
+        command = new Unsupported(view);
+    }
+    @Test
+    public void testCanProcess() {
+        // when
+        boolean canP = command.canProcess("");
+        //then
+        assertTrue(canP);
+    }
+
+    @Test
+    public void testProcess() {
+        // when
+        command.process("wrongCom");
+        verify(view).write("Несуществующая команда: wrongCom");
+
+    }
+
+}
