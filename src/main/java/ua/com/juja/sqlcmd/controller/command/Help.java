b1a0859b19b7731c579f1d5948f91eb4a0f8442c
Sergio <buchasergey03@gmail.com>
2016-08-30 16:42:13 +0300

    Сделал, CreateTable, CreateDB, DropTable, DropDB и тесты к ним

--- src/main/java/ua/com/juja/sqlcmd/controller/command/Help.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/command/Help.java
@@ -31,17 +31,29 @@ public class Help implements Command {
         view.write(ANSI_BLUE + "\tconnect|databaseName|userName|password");
         view.write(ANSI_RESET + "\t\tдля подключения к базе данных, с которой будем работать");
 
+        view.write(ANSI_BLUE + "\tcreateDB|databaseName");
+        view.write(ANSI_RESET + "\t\tдля создания база данных");
+
+        view.write(ANSI_BLUE + "\tdropDB|databaseName");
+        view.write(ANSI_RESET + "\t\tдля удаления база данных");
+
         view.write(ANSI_BLUE + "\ttables");
         view.write(ANSI_RESET + "\t\tдля получения списка всех таблиц базы, к которой подключились");
+
+        view.write(ANSI_BLUE + "\tcreateTable|tableName(name text, pass text, id serial, PRIMARY KEY(id)");
+        view.write(ANSI_RESET + "\t\tдля создания таблицы. ");
+
+        view.write(ANSI_BLUE + "\tcreateEntry|tableName|column1|value1|column2|value2|...|columnN|valueN");
+        view.write(ANSI_RESET + "\t\tдля создания записи в таблице");
 
         view.write(ANSI_BLUE + "\tclear|tableName");
         view.write(ANSI_RESET + "\t\tдля очистки всей таблицы");
 
-        view.write(ANSI_BLUE + "\tcreate|tableName|column1|value1|column2|value2|...|columnN|valueN");
-        view.write(ANSI_RESET + "\t\tдля создания записи в таблице");
+        view.write(ANSI_BLUE + "\tdropTable|tableName");
+        view.write(ANSI_RESET + "\t\tдля удаления таблицы таблицы");
 
         view.write(ANSI_BLUE + "\tshow|tableName");
-        view.write(ANSI_RESET + "\t\tдля получения содержимого таблицы 'tableName'");
+        view.write(ANSI_RESET + "\t\tдля получения содержимого таблицы");
 
         view.write(ANSI_BLUE + "\thelp");
         view.write(ANSI_RESET + "\t\tдля вывода этого списка на экран");
