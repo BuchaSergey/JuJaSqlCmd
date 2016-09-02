d82e28411907c5ce248253bd45092a33ba2abf04
Sergio <buchasergey03@gmail.com>
2016-09-02 22:27:09 +0300

    Добавил 100% тесты для getDatabasesNames, disconnectFromDb

---
+++ src/test/java/ua.com.juja.sqlcmd/controller/command/DisconnectFromDBTest.java
@@ -0,0 +1,43 @@
+package ua.com.juja.sqlcmd.controller.command;
+
+import org.junit.Before;
+import org.junit.Test;
+import ua.com.juja.sqlcmd.model.DatabaseManager;
+import ua.com.juja.sqlcmd.view.View;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by Seriy on 02.09.2016.
+ */
+public class DisconnectFromDBTest  {
+
+    private DatabaseManager manager;
+    private View view;
+    private Command command;
+
+    @Before
+    public void setup() {
+        manager = mock(DatabaseManager.class);
+        view = mock(View.class);
+        command = new DisconnectFromDB(manager, view);
+    }
+
+    @Test
+    public void testCanProcess() {
+        // when
+        boolean create = command.canProcess("disconnect");
+        assertTrue(create);
+    }
+
+    @Test
+    public void testWithConfirmDropDB() {
+        //when
+        command.process("disconnect");
+        //then
+        verify(manager).disconnectFromDB();
+        verify(view).write("Disconnected");
+    }
+}
---
+++ src/test/java/ua.com.juja.sqlcmd/controller/command/GetDatabasesNamesTest.java
@@ -0,0 +1,53 @@
+package ua.com.juja.sqlcmd.controller.command;
+
+import org.junit.Before;
+import org.junit.Test;
+import ua.com.juja.sqlcmd.model.DatabaseManager;
+import ua.com.juja.sqlcmd.view.View;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Seriy on 02.09.2016.
+ */
+public class GetDatabasesNamesTest {
+    private DatabaseManager manager;
+    private View view;
+    private Command command;
+
+    @Before
+    public void setup() {
+        manager = mock(DatabaseManager.class);
+        view = mock(View.class);
+        command = new GetDatabasesNames(manager, view);
+    }
+
+    @Test
+    public void testCanProcess() {
+        // when
+        boolean create = command.canProcess("databases");
+        assertTrue(create);
+    }
+
+    @Test
+    public void testWithConfirmDropDB() {
+        Set<String> databases = new LinkedHashSet<>(Arrays.asList("database1", "database2", "database3"));
+        when(manager.getDatabasesNames()).thenReturn(databases);
+        //when
+        command.process("databases");
+        //then
+        verify(manager).getDatabasesNames();
+        verify(view).write("____БАЗЫ____");
+        for (String database: databases) {
+            verify(view).write(database);
+        }
+
+    }
+}
