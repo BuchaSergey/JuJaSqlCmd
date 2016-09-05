6a55038a206d311b4051fc48df1adb0f5a0ef794
Sergio <buchasergey03@gmail.com>
2016-09-05 18:26:29 +0300

    добавил свой ексепшн

---
+++ src/main/java/ua/com/juja/sqlcmd/model/DatabaseManagerException.java
@@ -0,0 +1,10 @@
+package ua.com.juja.sqlcmd.model;
+
+/**
+ * Created by Seriy on 05.09.2016.
+ */
+public class DatabaseManagerException extends RuntimeException {
+    public DatabaseManagerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
