03c5ab8a18fc7532d1c95301e1b43c8d8de6d5d3
Sergio <buchasergey03@gmail.com>
2016-09-25 10:30:52 +0300

    1
    new

---
+++ src/main/java/ua/com/juja/sqlcmd/controller/command/DisconnectFromDB.java
@@ -0,0 +1,27 @@
+package ua.com.juja.sqlcmd.controller.command;
+
+
+import ua.com.juja.sqlcmd.model.DatabaseManager;
+import ua.com.juja.sqlcmd.view.View;
+
+public class DisconnectFromDB implements Command {
+
+    private DatabaseManager manager;
+    private View view;
+    public DisconnectFromDB(DatabaseManager manager, View view) {
+        this.manager = manager;
+        this.view = view;
+    }
+
+    @Override
+    public boolean canProcess(String command) {
+        return command.equals("disconnect");
+    }
+
+    @Override
+    public void process(String command) {
+
+        manager.disconnectFromDB();
+        view.write("Disconnected");
+    }
+}
---
+++ src/main/java/ua/com/juja/sqlcmd/controller/command/GetDatabasesNames.java
@@ -0,0 +1,29 @@
+package ua.com.juja.sqlcmd.controller.command;
+
+import ua.com.juja.sqlcmd.model.DatabaseManager;
+import ua.com.juja.sqlcmd.view.View;
+
+
+public class GetDatabasesNames implements Command {
+
+    private DatabaseManager manager;
+    private View view;
+
+    public GetDatabasesNames(DatabaseManager manager, View view) {
+        this.manager = manager;
+        this.view = view;
+    }
+
+    @Override
+    public boolean canProcess(String command) {
+        return command.equals("databases");
+    }
+
+    @Override
+    public void process(String command) {
+        view.write("____БАЗЫ____");
+        for (String database : manager.getDatabasesNames()) {
+            view.write(database);
+        }
+    }
+}
